a6a25ad0ce4aadc67d3fa408e53da08a07aede0c
GalenWong <wonggalen1999@gmail.com>
2020-10-18 23:33:04 +0800

    obtain referenced to the elements

--- session-3-dom-api/demo-product/script.js
+++ session-3-dom-api/demo-product/script.js
@@ -1,2 +1,9 @@
 console.log("If you see this in browser, that means our script is running.");
 
+const myClockFace = document.querySelector("#clock-face");
+const myButton = document.querySelector(".btn");
+
+console.log("Here is the clock face element:");
+console.log(myClockFace);
+console.log("Here is the button element:");
+console.log(myButton);
