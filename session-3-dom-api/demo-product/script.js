3fbb774980bc5862ee6518ac44753965f456ea15
GalenWong <wonggalen1999@gmail.com>
2020-10-18 23:38:53 +0800

    manipulate element

--- session-3-dom-api/demo-product/script.js
+++ session-3-dom-api/demo-product/script.js
@@ -3,6 +3,10 @@ console.log("If you see this in browser, that means our script is running.");
 const myClockFace = document.querySelector("#clock-face");
 const myButton = document.querySelector(".btn");
 
+myClockFace.innerHTML = "23:59:59";
+myButton.style.color = "red";
+myButton.style.fontSize = "2em";
+
 console.log("Here is the clock face element:");
 console.log(myClockFace);
 console.log("Here is the button element:");
